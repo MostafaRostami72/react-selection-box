a67f4cf23d140acbe025951f4ebb05e1c2874120
mostafa <m.rostamimahmodi72@gmail.com>
2021-10-31 11:52:25 +0330

    fix typescript support

---
+++ react-selection-box.d.ts
@@ -0,0 +1,88 @@
+import * as React from 'react';
+
+type ITypes = 'single' | 'multiple' | 'range'
+
+interface IOptions {
+    value: number | string
+    label: number | string
+    checked: boolean
+}
+
+interface IValueAnsLabelShape {
+    value: number | string
+    label: number | string
+}
+
+interface IDefaultSelectedRange {
+    min?: IValueAnsLabelShape,
+    max?: IValueAnsLabelShape
+}
+
+interface ITranslates {
+    label?: {
+        from: string,
+        to: string
+    },
+    placeholder?: {
+        min: string,
+        max: string,
+        search: string,
+    },
+    button?: {
+        apply: string,
+        cancel: string,
+    },
+    loading?: string,
+    no_options?: string
+}
+
+export interface IReactSelectionBoxProps {
+    children?: React.ReactNode;
+
+    // select label
+    label: string;
+    // select tag name
+    name: string;
+    // selection type
+    type: ITypes;
+    //selection options
+    options: IOptions[];
+    // selection onChange
+    onChange: (data: {} | null) => void
+    // select container class name
+    className?: string
+    // in multiple type, you can select how many items can be displayed in the caption
+    // default = 3
+    countSelectedInCaption?: number
+    // rtl
+    // default false
+    rtl?: boolean
+    // searchable
+    // default true
+    searchable?: boolean
+    // cleanable
+    // default true
+    cleanable?: boolean
+    // loading
+    // default false
+    loading?: boolean
+    // disable selection
+    // default false
+    disabled?: boolean
+    // autocomplete
+    autocomplete?: boolean
+    // required if autocomplete is true
+    onChangeAutocomplete?: (pattern: string | null) => void
+    // when selection dropdown opened
+    onShow?: (show: boolean) => void
+    // when selection dropdown closed
+    onHide?: (show: boolean) => void
+    // in rang selection, you can add default selected rang
+    defaultSelectedRange?: IDefaultSelectedRange
+    // selection translate
+    translates?: ITranslates
+}
+
+// ReactSelectionBox component is the default export
+
+export default function Selection(props: IReactSelectionBoxProps)
